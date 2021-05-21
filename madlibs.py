db134cd8c98c21d85f23e5003d644a3d70a0d80a
MercybirungiS <mercyssbirungi@gmail.com>
2021-05-21 21:09:12 +0300

    madlibsgame

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+# madlibs game 
+name =input("Enter name:")
+hobby=input("Enter a hobby:")
+desert=input("Enter your favorite food:")
+
+print ("My name is  " + name)
+print("I love "+hobby)
+print("I love "+desert)
